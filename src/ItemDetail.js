9b6547f2361bc920e4d5588bafe6a76109f2ce51
Hien2k1 <ngochien6969@gmail.com>
2019-10-18 18:47:34 +0700

    react router

---
+++ src/ItemDetail.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function ItemDetail({ match }) {
+
+    useEffect(() => {
+      fetchItem();
+      
+    }, []);
+
+    const [item, setItem] = useState({
+      images:{}
+    });
+
+    const fetchItem = async () => {
+       const fetchItem = await fetch(
+          `https://fortnite-api.theapinetwork.com/item/get?id=${match.params.id}`
+      );
+       const item = await fetchItem.json();
+       setItem(item);
+      console.log(item);
+
+      }
+
+
+    return (
+      <div>
+        <h1>{item.name}</h1>
+        <img src={item.images.transparent} />
+      </div>
+    );
+}
+
+export default ItemDetail;
